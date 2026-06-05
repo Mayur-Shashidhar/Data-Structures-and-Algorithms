f10d6c1fe638e6eb751c068817cd8555d61ad189
S Mayur <Mayurmamatha9916@gmail.com>
2026-06-05 21:14:24 +0530

    Create bruteforce.py

---
+++ Arrays/Rotate Array/bruteforce.py
@@ -0,0 +1,10 @@
+class Solution(object):
+  def rotate(self, nums, k):
+    n = len(nums)
+    temp = [0] * n
+
+    for i in range(n):
+      temp[(i+k)%n] = nums[i]
+
+    for i in range(n):
+      nums[i] = temp[i]
