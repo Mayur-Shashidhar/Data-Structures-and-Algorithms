0fc320e1745ad6f9c82beb508febd05caedc43b3
S Mayur <Mayurmamatha9916@gmail.com>
2026-06-09 21:57:48 +0530

    Create bruteforce.py

---
+++ Arrays/Set Matrix Zeroes/bruteforce.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        zero_positions = []
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    zero_positions.append((i, j))
+
+        for row, col in zero_positions:
+            for j in range(cols):
+                matrix[row][j] = 0
+            for i in range(rows):
+                matrix[i][col] = 0
