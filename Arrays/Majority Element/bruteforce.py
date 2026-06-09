f0dc1ad59d3073bd5a4a88ac676986ba80760f92
S Mayur <Mayurmamatha9916@gmail.com>
2026-06-09 18:54:44 +0530

    Create bruteforce.py

---
+++ Arrays/Majority Element/bruteforce.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        n = len(nums)
+
+        for i in range(n):
+            count = 0
+
+            for j in range(n):
+                if nums[j] == nums[i]:
+                    count += 1
+
+            if count > n // 2:
+                return nums[i]
