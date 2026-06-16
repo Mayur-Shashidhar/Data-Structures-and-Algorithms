1e3a62106fad908f0cae34bf9024206c54740609
S Mayur <Mayurmamatha9916@gmail.com>
2026-06-16 18:42:44 +0530

    Create main.py

---
+++ Hashing/Top K Frequent Elements/main.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+        answer = []
+
+        for i in range(k):
+            answer.append(sorted_freq[i][0])
+
+        return answer
