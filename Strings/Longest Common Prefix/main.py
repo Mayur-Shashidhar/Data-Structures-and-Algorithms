48921b0f2eadf18b4d6b94d4b40efd792cccdfdd
S Mayur <Mayurmamatha9916@gmail.com>
2026-06-18 12:17:54 +0530

    Create main.py

---
+++ Strings/Longest Common Prefix/main.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        first = strs[0]
+
+        for i in range(len(first)):
+            for s in strs[1:]:
+                if i >= len(s) or s[i] != first[i]:
+                    return first[:i]
+
+        return first
